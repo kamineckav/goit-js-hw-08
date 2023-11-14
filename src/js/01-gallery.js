6025e4d47e8a39808035ee6cc7e6670894866cc2
Viktoriia <128598090+kamineckav@users.noreply.github.com>
2023-11-14 11:33:51 +0100

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,14 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { createMarkup } from './helpers';
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
